92eaa08f5cbd82859b1537f7664a247fc862ddb4
Ben-Bingham <ben@benbingham.ca>
2026-02-17 19:53:36 -0700

    Finalized sphere intersection assembly

--- src/ShaderAssembly/SphereIntersect.asm
+++ src/ShaderAssembly/SphereIntersect.asm
@@ -67,132 +67,106 @@ setComponent(v0, s4, s5)	# v0.z = s4
 load(s5, 3)					# s5 = 3.0
 setComponent(v0, z, s5)		# v0.w = 0.0
 	
-
-
-############### START DEBUG PRINTING
-getComponent(v0, s0, z)
-setComponent(c, s0, z)
-
-load(s5, 1)
-
-getComponent(v0, s0, s5)
-setComponent(c, s0, s5)
-
-############### END DEBUG PRINTING
-
-
-
-
-
-
-
 # Define sphere, s0 is the z coordinate
-#s0 = 2.0;
-#s0 = -s0;
+load(s0, 2)					# s0 = 2.0
+negate(s0, s0)				# s0 = -s0
 
+# Compute a (from quadratic formula), stored in s1
+getComponent(v0, s1, z)		# s1 = v0.x
+multiply(s1, s1, s1)		# s1 = s1 * s1
+load(s5, 1)					# s5 = 1.0
+getComponent(v0, s2, s5)	# s2 = v0.y
+multiply(s2, s2, s2)		# s2 = s2 * s2
+load(s5, 2)					# s5 = 2.0
+getComponent(v0, s3, s5)	# s3 = v0.z
+multiply(s3, s3, s3)		# s3 = s3 * s3
+add(s1, s2, s1)				# s1 = s1 + s2
+add(s1, s3, s1)				# s1 = s1 + s3
+	
+# Compute b (from quadratic formula), stored in s2
+load(s2, 2)					# s2 = 2.0
+negate(s2, s2)				# s2 = -s2
+load(s5, 2)					# s5 = 2.0
+getComponent(v0, s3, s5)	# s3 = v0.z
+multiply(s2, s3, s2)		# s2 = s2 * s3
+multiply(s2, s0, s2)		# s2 = s2 * s0
 
+# Compute c (from quadratic formula), stored in s3
+load(s3, 1)					# s3 = 1.0
+negate(s3, s3)				# s3 = -s3
+multiply(s0, s0, s4)		# s4 = s0 * s0
+add(s4, s3, s3)				# s3 = s4 + s3
 
+# Compute discriminant, store in s0
+multiply(s2, s2, s4)		# s4 = s2 * s2
+load(s5, 4)					# s5 = 4.0
+multiply(s5, s1, s5)		# s5 = s5 * s1
+multiply(s5, s3, s5)		# s5 = s5 * s3
+negate(s5, s5)				# s5 = -s5
+add(s4, s5, s0)				# s0 = s4 + s5
 
+# Using: max(a, b) = 0.5 * (a + b + |a - b|)
 
+# Clamp discriminant first time
+# Set s1 = max(s0, 0.0)
+sign(s0, s1)				# s1 = sign(s0)
+multiply(s1, s0, s1)		# s1 = s1 * s0
+add(s1, s0, s1)				# s1 = s1 + s0
+load(s2, 2)					# s2 = 2.0
+reciprocal(s2, s2)			# s2 = 1.0 / 2.0
+multiply(s2, s1, s1)		# s1 = s2 * s1
 
-#	
-## Compute a (from quadratic formula), stored in s1
-#s1 = v0[int(floor(z))];
-#s1 = s1 * s1;
-#load(s5, 1) # s5 = 1.0
-#s2 = v0[int(floor(s5))];
-#s2 = s2 * s2;
-#s5 = 2.0;
-#s3 = v0[int(floor(s5))];
-#s3 = s3 * s3;
-#s1 = s1 + s2;
-#s1 = s1 + s3;
-#	
-## Compute b (from quadratic formula), stored in s2
-#s2 = 2.0;
-#s2 = -s2;
-#s5 = 2.0;
-#s3 = v0[int(floor(s5))];
-#s2 = s2 * s3;
-#s2 = s2 * s0;
-#	
-## Compute c (from quadratic formula), stored in s3
-#s3 = 1.0;
-#s3 = -s3;
-#s4 = s0 * s0;
-#s3 = s4 + s3;
-#	
-## Compute discriminant, store in s0
-#s4 = s2 * s2;
-#s5 = 4.0;
-#s5 = s5 * s1;
-#s5 = s5 * s3;
-#s5 = -s5;
-#s0 = s4 + s5;
-#	
-## Using: max(a, b) = 0.5 * (a + b + |a - b|)
-#	
-## Clamp discriminant first time
-## Set s1 = max(s0, 0.0)
-#s1 = sign(s0);
-#s1 = s1 * s0;
-#s1 = s1 + s0;
-#s2 = 2.0;
-#s2 = 1.0 / 2.0;
-#s1 = s2 * s1;
-#	
-## Set s2 = min(s1, 1.0) = -max(-s1, -1.0)
-#s4 = 1.0;
-#s1 = -s1;
-#s3 = s4 + s1;
-#s2 = sign(s3);
-#s2 = s2 * s3;
-#s4 = -s4;
-#s3 = s4 + s2;
-#s3 = s3 + s1;
-#s0 = s3;
-#s0 = -s0;
-#s1 = 2.0;
-#s1 = 1.0 / s1;
-#s0 = s0 * s1;
-#	
-## Take all values greater then 0, and ensure they are greater then 1
-#s1 = 100.0;
-#s0 = s0 * s1;
-#	
-## Clamp discriminant second time
-## Set s1 = max(s0, 0.0)
-#s1 = sign(s0);
-#s1 = s1 * s0;
-#s1 = s1 + s0;
-#s2 = 2.0;
-#s2 = 1.0 / 2.0;
-#s1 = s2 * s1;
-#	
-## Set s2 = min(s1, 1.0) = -max(-s1, -1.0)
-#s4 = 1.0;
-#s1 = -s1;
-#s3 = s4 + s1;
-#s2 = sign(s3);
-#s2 = s2 * s3;
-#s4 = -s4;
-#s3 = s4 + s2;
-#s3 = s3 + s1;
-#s0 = s3;
-#s0 = -s0;
-#s1 = 2.0;
-#s1 = 1.0 / s1;
-#s0 = s0 * s1;
-#	
-#load(s1, 1) # s1 = 1.0
-#s2 = 2.0;
-#s3 = 3.0;
-#
-#outFragColor[int(floor(z))] = s0;
-#outFragColor[int(floor(s1))] = s0;
-#outFragColor[int(floor(s2))] = s0;
-#outFragColor[int(floor(s3))] = 1.0;
+# Set s2 = min(s1, 1.0) = -max(-s1, -1.0)
+load(s4, 1)					# s4 = 1.0
+negate(s1, s1)				# s1 = -s1
+add(s4, s1, s3)				# s3 = s4 + s1
+sign(s3, s2)				# s2 = sign(s3)
+multiply(s2, s3, s2)		# s2 = s2 * s3
+negate(s4, s4)				# s4 = -s4
+add(s4, s2, s3)				# s3 = s4 + s2
+add(s3, s1, s3)				# s3 = s3 + s1
+move(s3, s0)				# s0 = s3
+negate(s0, s0)				# s0 = -s0
+load(s1, 2)					# s1 = 2.0
+reciprocal(s1, s1)			# s1 = 1.0 / s1
+multiply(s0, s1, s0)		# s0 = s0 * s1
 
+# Take all values greater then 0, and ensure they are greater then 1
+load(s1, 100)				# s1 = 100.0
+multiply(s0, s1, s0)		# s0 = s0 * s1
+
+# Clamp discriminant second time
+# Set s1 = max(s0, 0.0)
+sign(s0, s1)				# s1 = sign(s0)
+multiply(s1, s0, s1)		# s1 = s1 * s0
+add(s1, s0, s1)				# s1 = s1 + s0
+load(s2, 2)					# s2 = 2.0
+reciprocal(s2, s2)			# s2 = 1.0 / 2.0
+multiply(s2, s1, s1)		# s1 = s2 * s1
+
+# Set s2 = min(s1, 1.0) = -max(-s1, -1.0)
+load(s4, 1)					# s4 = 1.0
+negate(s1, s1)				# s1 = -s1
+add(s4, s1, s3)				# s3 = s4 + s1
+sign(s3, s2)				# s2 = sign(s3)
+multiply(s2, s3, s2)		# s2 = s2 * s3
+negate(s4, s4)				# s4 = -s4
+add(s4, s2, s3)				# s3 = s4 + s2
+add(s3, s1, s3)				# s3 = s3 + s1
+move(s3, s0)				# s0 = s3
+negate(s0, s0)				# s0 = -s0
+load(s1, 2)					# s1 = 2.0
+reciprocal(s1, s1)			# s1 = 1.0 / s1
+multiply(s0, s1, s0)		# s0 = s0 * s1
+
+load(s1, 1)					# s1 = 1.0
+load(s2, 2)					# s2 = 2.0
+load(s3, 3)					# s3 = 3.0
+
+setComponent(c, s0, z)		# c.x = s0
+setComponent(c, s0, s1)		# c.y = s0
+setComponent(c, s0, s2)		# c.y = s0
+setComponent(c, s1, s3)		# c.y = s1
 
 ######################################################################################
 # What follows are the GLSL shaders I wrote while writing the above assembly version #
